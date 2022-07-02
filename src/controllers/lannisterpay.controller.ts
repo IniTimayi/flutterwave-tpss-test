12f979baa9d0ffd833a8d4958ef1275a58d53397
IniTimayi <initimayi@gmail.com>
2022-07-03 00:31:50 +0100

    swapped deplpoyment path from vercel to heroku

--- src/controllers/lannisterpay.controller.ts
+++ src/controllers/lannisterpay.controller.ts
@@ -1,28 +1,30 @@
 import { Request, Response } from 'express'
 import {LannisterPayload, SplitInfo} from "../types/lannisterPayTypes";
 
-export const paymentComputation = async (req: Request, res: Response) => {
-    const {ID, SplitInfo, Amount} = req.body as LannisterPayload
-    const sortedSplitInfoArray = SplitInfo.map((items) => {
-        if (items.SplitType === 'FLAT') {
-            return {...items, weight: 1}
-        } else if (items.SplitType === 'PERCENTAGE') {
-            return {...items, weight: 2}
-        } else {
-            return {...items, weight: 3}
-        }
-    }).sort((a, b) => {
-        return a.weight < b.weight ? -1 : 1
-    })
 
-    const modifiedArray = sortedSplitInfoArray.map((item) => {
-        const {weight, ...other} = item
-        return other
-    })
 
-    const computedValue = await compute({ID, initialAmount: Amount, splitInfoArray: modifiedArray})
-    res.status(200).json(computedValue)
-}
+    export const paymentComputation = async (req: Request, res: Response) => {
+        const {ID, SplitInfo, Amount} = req.body as LannisterPayload
+        const sortedSplitInfoArray = SplitInfo.map((items) => {
+            if (items.SplitType === 'FLAT') {
+                return {...items, weight: 1}
+            } else if (items.SplitType === 'PERCENTAGE') {
+                return {...items, weight: 2}
+            } else {
+                return {...items, weight: 3}
+            }
+        }).sort((a, b) => {
+            return a.weight < b.weight ? -1 : 1
+        })
+
+        const modifiedArray = sortedSplitInfoArray.map((item) => {
+            const {weight, ...other} = item
+            return other
+        })
+
+        const computedValue = await compute({ID, initialAmount: Amount, splitInfoArray: modifiedArray})
+        res.status(200).json(computedValue)
+    }
 
     interface ComputePayload{
         ID: number
@@ -32,7 +34,7 @@ export const paymentComputation = async (req: Request, res: Response) => {
     const compute = async (payload: ComputePayload) => {
         const {initialAmount, splitInfoArray, ID} = payload
         let computationBalance = initialAmount
-        const totalRatio = splitInfoArray.reduce((acc:number, item: SplitInfo) => {
+        const totalRatio = splitInfoArray.reduce((acc, item) => {
           return item.SplitType === 'RATIO' ? acc + item.SplitValue : 0
         },0)
         let tempRatioBalance = initialAmount
